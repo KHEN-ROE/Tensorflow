bb967e5cd95d802743be4a0077cdf66be6160dc6
KEN ROE <shdnrnjs@gmail.com>
2023-04-17 18:05:24 +0900

    init

---
+++ DeepLearning2020/py/0301.py
@@ -0,0 +1,23 @@
+from tensorflow.python.framework.ops import disable_eager_execution
+import tensorflow as tf
+
+
+print(tf.executing_eagerly())
+disable_eager_execution()
+print(tf.executing_eagerly())
+
+# Graph construction
+a = tf.constant(1) # 정수형 텐서를 생성
+b = tf.constant(2)
+c = a + b  # c= tf.add(a, b)
+print(a)
+print(b)
+print(c)
+
+
+#2 graph execution
+sess = tf.compat.v1.Session()
+print(sess.run(a))
+print(sess.run(b))
+print(sess.run(c))
+sess.close()
